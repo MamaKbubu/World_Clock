213a1f1818117ad10a6d3f441b974a207e6f4965
MamaKbubu <maboelatk@gmail.com>
2025-03-01 21:28:51 +0200

    johannesburg

--- javacript/src.js
+++ javacript/src.js
@@ -1,1 +1,15 @@
-alert("hello world clock");
+function updateTime() {
+  //for Johannesburg
+  let johannesburgElement = document.querySelector("johannesburg");
+  if (johannesburgElement) {
+    let johannesburgDateElement = johannesburgElement.querySelector(".date");
+    let johannesburgTimeElement = johannesburgElement.querySelector(".time");
+    let johannesburgTime = moment().tz("Africa/Johannesburg");
+
+    johannesburgDateElement.innerHTML =
+      johannesburgTime.format("dddd, MMMM Do YYYY");
+    johannesburgTimeElement.innerHTML = johannesburgTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
