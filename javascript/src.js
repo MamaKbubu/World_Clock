473b0869de3000a236168488ecc900d6895ed782
MamaKbubu <maboelatk@gmail.com>
2025-03-02 15:30:51 +0200

    this was very tough

--- javascript/src.js
+++ javascript/src.js
@@ -1,48 +1,54 @@
 function updateTime() {
-  let cities = document.querySelectorAll(".city");
-  cities.forEach((cityElement) => {
-    let timeZone = cityElement.getAttribute("data-timezone");
-    let cityTime = moment().tz(timeZone);
+  // Johannesburg
+  let johannesburgElement = document.querySelector("#johannesburg");
+  if (johannesburgElement) {
+    let johannesburgDateElement = johannesburgElement.querySelector(".date");
+    let johannesburgTimeElement = johannesburgElement.querySelector(".time");
+    let johannesburgTime = moment().tz("Africa/Johannesburg");
 
-    let dateElement = cityElement.querySelector(".date");
-    let timeElement = cityElement.querySelector(".time");
+    johannesburgDateElement.innerHTML = johannesburgTime.format("MMMM Do YYYY");
+    johannesburgTimeElement.innerHTML = johannesburgTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
-    dateElement.innerHTML = cityTime.format("MMMM Do YYYY");
-    timeElement.innerHTML = cityTime.format("h:mm:ss [<small>]A[</small>]");
-  });
+  // Lagos
+  let lagosElement = document.querySelector("#lagos");
+  if (lagosElement) {
+    let lagosDateElement = lagosElement.querySelector(".date");
+    let lagosTimeElement = lagosElement.querySelector(".time");
+    let lagosTime = moment().tz("Africa/Lagos");
+
+    lagosDateElement.innerHTML = lagosTime.format("MMMM Do YYYY");
+    lagosTimeElement.innerHTML = lagosTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
   if (!cityTimeZone) return;
 
-  let cityName = cityTimeZone.split("/")[1].replace("_", " ");
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
-
   let citiesElement = document.querySelector("#cities");
 
-  // Check if city is already displayed
-  let existingCity = document.querySelector(
-    `.city[data-timezone="${cityTimeZone}"]`
-  );
-  if (existingCity) return;
-
-  let newCityElement = document.createElement("div");
-  newCityElement.classList.add("city");
-  newCityElement.setAttribute("data-timezone", cityTimeZone);
-  newCityElement.innerHTML = `
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
       <h2>${cityName}</h2>
       <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
-      <div class="time">${cityTime.format("h:mm:ss [<small>]A[</small>]")}</div>
-    `;
-
-  citiesElement.appendChild(newCityElement);
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
 }
 
-// Initial time update and refresh every second
 updateTime();
 setInterval(updateTime, 1000);
 
-// Listen for city selection
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
